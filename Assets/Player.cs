484e2962638c83d3a65ed7045f8abb88fe377b0d
ShivangNagta <shivangnag@gmail.com>
2023-10-24 15:41:13 +0530

    Added player control and screen size

---
+++ Assets/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10.0f;
+    float vel;
+    float screenHalfWidth;
+    
+
+    void Start()
+    {
+        float halfPlayerWidth = transform.localScale.x / 2f;
+        screenHalfWidth = Camera.main.aspect*Camera.main.orthographicSize+halfPlayerWidth;
+    }
+    void Update()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+        vel = input * speed;
+
+        if (transform.position.x < -screenHalfWidth)
+        {
+            transform.position = new Vector2(screenHalfWidth, transform.position.y);
+        }
+
+        if (transform.position.x > screenHalfWidth)
+        {
+            transform.position = new Vector2(-screenHalfWidth, transform.position.y);
+        }
+
+    }
+    void FixedUpdate()
+    {
+        transform.Translate(Vector2.right * vel * Time.deltaTime);
+    }
+}
